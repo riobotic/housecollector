2b523c6e0623bb5023a209111fcfa6b5818ee8b1
Daniel JS <daniel@mydeveloperjourney.com>
2022-02-16 19:50:26 -0600

    wip: adds latest updates

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,5 @@
 from django.shortcuts import render
-
-"""
-This is our fake database
-"""
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# here's our list of cats
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4),
-]
-
+from .models import Cat
 
 def home(request):
     return render(request, 'home.html')
@@ -25,4 +8,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_detail(request, cat_id):
+   cat = Cat.objects.get(id=cat_id)
+   return render(request, 'cats/detail.html', {'cat': cat })
