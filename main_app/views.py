2210102731b615820dbd054498a6a049aa3a7876
Daniel JS <daniel@mydeveloperjourney.com>
2022-02-22 19:49:49 -0600

    wip: adds latest updates

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
+from pyexpat import model
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 
 def home(request):
@@ -16,9 +17,25 @@ def cats_detail(request, cat_id):
    cat = Cat.objects.get(id=cat_id)
    return render(request, 'cats/detail.html', {'cat': cat })
 
+# Renders a template with a form on it
+# Creates a model form based on the model
+# Responds to GET and POST requests
+#  1) GET render the new cat form
+#  2) POST submit the form to create a new instance
+# Validate form inputs
+# Handles the necessary redirect following a model instance creating
+
 class CatCreate(CreateView):
     model = Cat
     # fields = ('name', 'breed', 'age', 'description')
     fields = '__all__'
     # success_url = '/cats/' this will work, but it's not preferred
     # Fat Models, Skinny Controllers
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ('name', 'breed', 'description', 'age')
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
