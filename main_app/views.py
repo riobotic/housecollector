f0620b3eddcea738894c9418604f2dbbe0116760
Daniel JS <daniel@mydeveloperjourney.com>
2022-02-23 21:06:21 -0600

    wip: adds latest change

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
-from pyexpat import model
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
 
 def home(request):
     return render(request, 'home.html')
@@ -15,7 +15,25 @@ def cats_index(request):
 
 def cats_detail(request, cat_id):
    cat = Cat.objects.get(id=cat_id)
-   return render(request, 'cats/detail.html', {'cat': cat })
+   feeding_form = FeedingForm()
+   return render(request, 'cats/detail.html', {
+       'cat': cat, 
+       'feeding_form': feeding_form 
+    })
+
+def add_feeding(request, cat_id):
+    # 1) collect form input values
+    form = FeedingForm(request.POST)
+    # 2) valid input values
+    if form.is_valid():
+        # 3) save a copy of a new feeding instance in memory
+        new_feeding = form.save(commit=False)
+        # 4) attach a reference to the cat that owns the feeding
+        new_feeding.cat_id = cat_id
+        # 5) save the new feeding to the database
+        new_feeding.save()
+    # 6) redirect the user back to the detail
+    return redirect('detail', cat_id=cat_id)
 
 # Renders a template with a form on it
 # Creates a model form based on the model
